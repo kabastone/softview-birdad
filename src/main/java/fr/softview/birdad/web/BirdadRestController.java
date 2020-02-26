773bf75f722500811f8c92d3d6c3aa64c695f684
kabastone <dareksar@gmail.com>
2020-02-26 18:41:42 +0100

    Implements search criteria

--- src/main/java/fr/softview/birdad/web/BirdadRestController.java
+++ src/main/java/fr/softview/birdad/web/BirdadRestController.java
@@ -1,35 +1,40 @@
 package fr.softview.birdad.web;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.facebook.ads.sdk.APIException;
 
-import fr.softview.birdad.domain.Advertisement;
 import fr.softview.birdad.domain.Response;
 
 @RestController
 public class BirdadRestController {
 
 	 private static final Logger log = LoggerFactory.getLogger(BirdadRestController.class);
+	 private static final String ACCESS_TOKEN = "EAAt4qpgBMEQBADNpGapJIE3mHx0y8WhYvkub4oppLjoh0ouSZA0JU3CpJY3R8WpvZA9Gr9Qfezy2TreGFRY6PPQ6YNZBqQvsusWhVJjjUozHL7LKuGiWWwVyWm1GZA70n2fLpDyGyFZAq2zg8vik3CWlYpK5qArafZB9CdpWRa5lpiZBFzrBrKuAwPejs3O29YZD";
   
 	@GetMapping("/birdads")
-	Response getAdvertices() throws APIException {
+	Response getAdvertices(@RequestParam() MultiValueMap<String, String> uriParams) throws APIException {
 		RestTemplate restTemplate = new RestTemplate();
-		String url = "https://graph.facebook.com/v6.0/ads_archive?ad_reached_countries={ad_reached_countries}&search_terms={search_terms}&access_token={access_token}";
+		uriParams.addIfAbsent("ad_reached_countries", "FR,US,SN");
+		uriParams.addIfAbsent("search_terms", "paris");
+		uriParams.add("access_token", ACCESS_TOKEN);
+
+		UriBuilder uri = UriComponentsBuilder.newInstance(
+				).scheme("https")
+				.host("graph.facebook.com")
+				.path("/v6.0/ads_archive")
+				.queryParams(uriParams);
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("ad_reached_countries", "FR");
-		uriVariables.put("search_terms", "paris");
-		uriVariables.put("access_token", "EAAt4qpgBMEQBADiuWGEOjdy2qb4N4a0iUhcUk7UCim0EfaKi6S3vFKUxoaOmc6BqC4Vp49LViwjRE2n9xd1RN41WYCoY0VbBQE5QotgddGBKuKlVgNv3i2ryiNODYx8QhiHyEcaDDW8F3JnbqzZBRPb5HiZAc2deilXzVVmNKJHTfEKqLJ");
-		
-		Response response = restTemplate.getForObject(url, Response.class, uriVariables);
+		log.info("URI " + uri.build().toString());
+		Response response = restTemplate.getForObject(uri.build().toString(), Response.class);
 		log.info("======== REST API WITH REST TEMPLATE============================== ");
 		log.info(response.toString());
         return response ;
